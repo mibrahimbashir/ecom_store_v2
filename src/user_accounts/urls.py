dbc4c46f8cfa53cc9dad123be5172976a20338b0
ibrahim bashir <ibrahimbashir370@gmail.com>
2025-04-06 08:44:24 +0500

    Implement user info update functionality; add view for handling user info updates and integrate form action in user info template

--- src/user_accounts/urls.py
+++ src/user_accounts/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('profile/', views.user_profile, name='profile'),
     path('user_info_form/', views.user_info_form, name='user_info_form'),
+    path('update_user_info/', views.update_user_info, name='update_user_info'),
 ]
--- src/user_accounts/views.py
+++ src/user_accounts/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.views.decorators.http import require_POST
+from django.db import DatabaseError
 
 
 @login_required(login_url='login')
@@ -14,5 +17,27 @@ def user_profile(request):
     return render(request, 'user_accounts/user_profile.html', context)
 
 
+@login_required(login_url='login')
 def user_info_form(request):
     return render(request, 'user_accounts/user_info_form.html', {})
+
+
+@login_required(login_url='login')
+@require_POST
+def update_user_info(request):
+    user = request.user
+    first_name = request.POST.get("first_name", "").strip()
+    last_name = request.POST.get("last_name", "").strip()
+
+    try:
+        if len(first_name) > 20 or len(last_name) > 20:
+            messages.error(request, 'Bad Reqest')
+            return redirect('profile')
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+
+        return redirect('profile')
+
+    except DatabaseError as e:
+        return redirect('profile')
